424ee4644e4b238872384fd1fa6751fd069f034e
A1cy <a1hvdy@gmail.com>
2024-02-04 11:54:35 +0300

    A

--- src/pages/dashboards/ecommerce.vue
+++ src/pages/dashboards/ecommerce.vue
@@ -1,105 +1,106 @@
 <script setup>
-    import EcommerceCongratulationsJohn from '@/views/dashboards/ecommerce/EcommerceCongratulationsJohn.vue'
-    import EcommerceEarningReports from '@/views/dashboards/ecommerce/EcommerceEarningReports.vue'
-    import EcommerceExpensesRadialBarCharts from '@/views/dashboards/ecommerce/EcommerceExpensesRadialBarCharts.vue'
-    import EcommerceGeneratedLeads from '@/views/dashboards/ecommerce/EcommerceGeneratedLeads.vue'
-    import EcommerceInvoiceTable from '@/views/dashboards/ecommerce/EcommerceInvoiceTable.vue'
-    import EcommerceOrder from '@/views/dashboards/ecommerce/EcommerceOrder.vue'
-    import EcommercePopularProducts from '@/views/dashboards/ecommerce/EcommercePopularProducts.vue'
-    import EcommerceRevenueReport from '@/views/dashboards/ecommerce/EcommerceRevenueReport.vue'
-    import EcommerceStatistics from '@/views/dashboards/ecommerce/EcommerceStatistics.vue'
-    import EcommerceTotalProfitLineCharts from '@/views/dashboards/ecommerce/EcommerceTotalProfitLineCharts.vue'
-    import EcommerceTransactions from '@/views/dashboards/ecommerce/EcommerceTransactions.vue'
-    import {
-        useSheetsDataStore
-    } from '@/@core/stores/sheetsData'
-    import CardStatisticsTransactions from '@/views/pages/cards/card-statistics/CardStatisticsTransactions.vue'
-    import yesNoPercentage from '@/views/apps/ecommerce/yesNoPercentage.vue'
-    import CcSupportTracker from '@/views/apps/ecommerce/CcSupportTracker.vue'
-    import datasheet from '@/views/apps/ecommerce/datasheet.vue'
+// import EcommerceCongratulationsJohn from '@/views/dashboards/ecommerce/EcommerceCongratulationsJohn.vue'
+// import EcommerceEarningReports from '@/views/dashboards/ecommerce/EcommerceEarningReports.vue'
+// import EcommerceExpensesRadialBarCharts from '@/views/dashboards/ecommerce/EcommerceExpensesRadialBarCharts.vue'
+// import EcommerceGeneratedLeads from '@/views/dashboards/ecommerce/EcommerceGeneratedLeads.vue'
+// import EcommerceInvoiceTable from '@/views/dashboards/ecommerce/EcommerceInvoiceTable.vue'
+// import EcommerceOrder from '@/views/dashboards/ecommerce/EcommerceOrder.vue'
+// import EcommercePopularProducts from '@/views/dashboards/ecommerce/EcommercePopularProducts.vue'
+// import EcommerceRevenueReport from '@/views/dashboards/ecommerce/EcommerceRevenueReport.vue'
+// import EcommerceStatistics from '@/views/dashboards/ecommerce/EcommerceStatistics.vue'
+// import EcommerceTotalProfitLineCharts from '@/views/dashboards/ecommerce/EcommerceTotalProfitLineCharts.vue'
+// import EcommerceTransactions from '@/views/dashboards/ecommerce/EcommerceTransactions.vue'
+import {
+  useSheetsDataStore,
+} from '@/@core/stores/sheetsData'
+import CardStatisticsTransactions from '@/views/pages/cards/card-statistics/CardStatisticsTransactions.vue'
+import yesNoPercentage from '@/views/apps/ecommerce/yesNoPercentage.vue'
+import CcSupportTracker from '@/views/apps/ecommerce/CcSupportTracker.vue'
+import datasheet from '@/views/apps/ecommerce/datasheet.vue'
 
 
-    const store = useSheetsDataStore()
+const store = useSheetsDataStore()
 
-    store.fetchSheetData() // Fetch data on component mount or when needed
+store.fetchSheetData() // Fetch data on component mount or when needed
 </script>
 
 <template>
-    <VRow class="match-height">
+  <VRow class="match-height">
+    <VCol cols="12">
+      <CardStatisticsTransactions />
+    </VCol>
 
-        <VCol cols="12">
-            <CardStatisticsTransactions />
-        </VCol>
-
-        <VCol cols="12" md="6">
-            <CcSupportTracker />
-        </VCol>
-        <VCol cols="12" md="6">
-            <VCard>
-
-                <yesNoPercentage />
-
-            </VCard>
-        </VCol>
-        <VCol cols="12">
-            <datasheet />
-        </VCol>
-        <!-- Integration of Google Sheets Data -->
-        <!-- 
-        <VCol cols="12" md="12" lg="12" />
-        <VCol cols="12" md="5" lg="4">
-            <EcommerceCongratulationsJohn />
-        </VCol>
+    <VCol
+      cols="12"
+      md="6"
+    >
+      <CcSupportTracker />
+    </VCol>
+    <VCol
+      cols="12"
+      md="6"
+    >
+      <VCard>
+        <yesNoPercentage />
+      </VCard>
+    </VCol>
+    <VCol cols="12">
+      <datasheet />
+    </VCol>
+    <!-- Integration of Google Sheets Data -->
+    <!-- 
+      <VCol cols="12" md="12" lg="12" />
+      <VCol cols="12" md="5" lg="4">
+      <EcommerceCongratulationsJohn />
+      </VCol>
 
  
-        <VCol cols="12" md="7" lg="8">
-            <EcommerceStatistics class="h-100" />
-        </VCol>
+      <VCol cols="12" md="7" lg="8">
+      <EcommerceStatistics class="h-100" />
+      </VCol>
 
-        <VCol cols="12" lg="4">
-            <VRow>
-                 <VCol cols="12" lg="6" md="3" sm="6">
-                    <EcommerceExpensesRadialBarCharts />
-                </VCol>
+      <VCol cols="12" lg="4">
+      <VRow>
+      <VCol cols="12" lg="6" md="3" sm="6">
+      <EcommerceExpensesRadialBarCharts />
+      </VCol>
 
-                 <VCol cols="12" lg="6" md="3" sm="6">
-                    <EcommerceTotalProfitLineCharts />
-                </VCol>
+      <VCol cols="12" lg="6" md="3" sm="6">
+      <EcommerceTotalProfitLineCharts />
+      </VCol>
 
-                 <VCol cols="12" md="6" lg="12">
-                    <EcommerceGeneratedLeads />
-                </VCol>
-            </VRow>
-        </VCol>
+      <VCol cols="12" md="6" lg="12">
+      <EcommerceGeneratedLeads />
+      </VCol>
+      </VRow>
+      </VCol>
 
-         <VCol cols="12" lg="8">
-            <EcommerceRevenueReport />
-        </VCol>
+      <VCol cols="12" lg="8">
+      <EcommerceRevenueReport />
+      </VCol>
 
-         <VCol cols="12" sm="6" lg="4">
-            <EcommerceEarningReports />
-        </VCol>
+      <VCol cols="12" sm="6" lg="4">
+      <EcommerceEarningReports />
+      </VCol>
 
-         <VCol cols="12" sm="6" lg="4">
-            <EcommercePopularProducts />
-        </VCol>
+      <VCol cols="12" sm="6" lg="4">
+      <EcommercePopularProducts />
+      </VCol>
 
-         <VCol cols="12" sm="6" lg="4">
-            <EcommerceOrder />
-        </VCol>
+      <VCol cols="12" sm="6" lg="4">
+      <EcommerceOrder />
+      </VCol>
 
-         <VCol cols="12" sm="6" lg="4">
-            <EcommerceTransactions />
-        </VCol>
+      <VCol cols="12" sm="6" lg="4">
+      <EcommerceTransactions />
+      </VCol>
 
-        <VCol cols="12" lg="8">
-            <EcommerceInvoiceTable />
-        </VCol> -->
-    </VRow>
+      <VCol cols="12" lg="8">
+      <EcommerceInvoiceTable />
+      </VCol> 
+    -->
+  </VRow>
 </template>
-
-
-
 
 <style lang="scss">
     @use "@core/scss/template/libs/apex-chart.scss";
